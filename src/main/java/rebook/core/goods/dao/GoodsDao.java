3c9178c193d7194a27d33a07a2220a473f804ee0
saiuday <saiuday3708@gmail.com>
2021-03-17 20:29:52 +0530

    shoes commit

---
+++ src/main/java/rebook/core/goods/dao/GoodsDao.java
@@ -0,0 +1,15 @@
+package rebook.core.goods.dao;
+
+import rebook.common.base.BaseDao;
+import rebook.core.goods.entity.GoodsEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Auther: Aaron
+ * @Date: 2018-08-29 11:41:54
+ * @Description: ${comment}
+ */
+@Mapper
+public interface GoodsDao extends BaseDao<GoodsEntity> {
+	
+}
---
+++ src/main/java/rebook/core/sys/dao/SysUserDao.java
@@ -0,0 +1,15 @@
+package rebook.core.sys.dao;
+
+import rebook.common.base.BaseDao;
+import rebook.core.sys.entity.SysUserEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Auther: Aaron
+ * @Date: 2018-08-29 11:37:56
+ * @Description: ${comment}
+ */
+@Mapper
+public interface SysUserDao extends BaseDao<SysUserEntity> {
+	
+}
